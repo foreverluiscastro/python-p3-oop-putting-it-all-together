778b1cce59760f162f3f0aa8a63c405e93bce420
Luis <foreverluiscastro@gmail.com>
2022-07-10 18:36:04 -0400

    Done.

--- lib/book.py
+++ lib/book.py
@@ -2,3 +2,36 @@
 
 class Book:
     pass
+    def __init__(self, title):
+        pass
+        self.title = title
+    
+    # practice the property() function for adding author_name, 
+    # page_count and genre properties to an instance of the Book class
+    # that means making a set and get method for author_name, page_count
+    # and genre and using the property() method to allow the mthods to be used
+
+    def get_author_name(self):
+        return self._author_name
+
+    def set_author_name(self, author_name):
+        self._author_name = author_name
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        self._page_count = page_count
+
+    def get_genre(self):
+        return self._genre
+
+    def set_genre(self, genre):
+        self._genre = genre
+
+    genre = property(get_genre, set_genre)
+    author_name = property(get_author_name, set_author_name)
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,47 @@
 
 class Shoe:
     pass
+    def __init__(self, brand):
+        pass
+        self.brand = brand
+
+    def get_color(self):
+        pass
+        return self._color
+
+    def set_color(self, color):
+        pass
+        self._color = color
+
+    def get_size(self):
+        pass
+        return self._size
+
+    def set_size(self, size):
+        pass
+        self._size = size
+
+    def get_material(self):
+        pass
+        return self._material
+
+    def set_material(self, material):
+        pass
+        self._material = material
+
+    def get_condition(self):
+        pass
+        return self._condition
+
+    def set_condition(self, condition):
+        pass
+        self._condition = condition
+
+    color = property(get_color, set_color)
+    size = property(get_size, set_size)
+    material = property(get_material, set_material)
+    condition = property(get_condition, set_condition)
+
+    def cobble(self):
+        self._condition = "New"
+        print("Your shoe is as good as new!")
